a3bbc8cff3f30b08977e53aa931db1fb71daec9d
Aydin Battal <battalay@sheridancollege.ca>
2021-02-24 22:10:32 -0500

    Implemented the functionality of adding teams to the database

--- src/main/java/ca/sheridancollege/battalay/controllers/TeamController.java
+++ src/main/java/ca/sheridancollege/battalay/controllers/TeamController.java
@@ -4,8 +4,10 @@ import ca.sheridancollege.battalay.database.DatabaseAccess;
 import ca.sheridancollege.battalay.model.Team;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class TeamController {
@@ -16,12 +18,26 @@ public class TeamController {
     ModelAndView mv;
 
     @GetMapping("/")
-    public ModelAndView index(){
+    public String index(){
 
-        mv = new ModelAndView("home", "teams",da.getTeams());
-        mv.addObject("team", new Team());
+//        mv = new ModelAndView("Home", "teams",da.getTeams());
+//        mv.addObject("team", new Team());
 
-        return mv;
+        return "/Home";
+    }
+
+    @RequestMapping("/addTeam") //make it PostMapping
+    public ModelAndView processTeam(HttpSession session, @ModelAttribute Team team){
+
+        if (team.getName() == null){
+            System.out.println("null");
+        }else {
+            da.insertTeam(team.getName(),team.getContinent(),team.getGamesPlayed(),
+                    team.getWins(),team.getDraws(),team.getLosses());
+//        session.setAttribute("team",team);
+        }
+
+        return new ModelAndView("AddTeam", "team",team);
     }
 
 }
--- src/main/java/ca/sheridancollege/battalay/database/DatabaseAccess.java
+++ src/main/java/ca/sheridancollege/battalay/database/DatabaseAccess.java
@@ -20,6 +20,22 @@ public class DatabaseAccess {
         String query = "SELECT * FROM Teams";
 
         return jdbc.query(query,namedParameters,new BeanPropertyRowMapper<Team>(Team.class));
+    }
+
+    public void  insertTeam(String name,String continent,int gamesPlayed,int wins,int draws,int losses){
+
+        MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+        String query = "INSERT INTO Teams(TeamName,Continent,Played,Won,Drawn,Lost) " +
+                "VALUES(:nam,:cont,:play,:win,:draw,:loss)";
+        namedParameters.addValue("nam" , name);
+        namedParameters.addValue("cont" , continent);
+        namedParameters.addValue("play" , gamesPlayed);
+        namedParameters.addValue("win" , wins);
+        namedParameters.addValue("draw" , draws);
+        namedParameters.addValue("loss" , losses);
+        int rowsAffected = jdbc.update(query,namedParameters);
+        if (rowsAffected > 0)
+            System.out.println("Team record was added successfully!");
 
     }
 }
